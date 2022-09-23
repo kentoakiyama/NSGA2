811409efc0ea9b991b0db160a23a22e9af4fd6c7
kento <akiyama09141203@gmail.com>
2022-09-23 14:51:53 +0900

    update

--- test.py
+++ test.py
@@ -2,24 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from nsga2.nsga2 import NSGA2
-from test_functions.osyczkakundu import OsyczkaKundu
-from test_functions.zdt import ZDT2, ZDT4
+from test_functions.get_problem import get_problem
 
+problem = get_problem('TNK')
 
-n_gen = 10
-pop_size = 8
+n_processes = 1
+n_gen = 100
+seed = 6
+pop_size = 128
 crossover_eta = 15
 mutation_prob = 0.
 mutation_eta = 20
 
-nsga2 = NSGA2(ZDT4, pop_size, n_gen, mutation_prob=mutation_prob, mutation_eta=mutation_eta, crossover_eta=crossover_eta)
+nsga2 = NSGA2(problem, pop_size, n_gen, mutation_prob=mutation_prob, mutation_eta=mutation_eta, crossover_eta=crossover_eta, n_processes=n_processes, seed=seed)
 result = nsga2.minimize()
-# import pdb;pdb.set_trace()
-# x = np.array(result.x)
-# f = np.array(result.f)
-# r = np.array(result.r)
-# 
-# fig, ax = plt.subplots()
-# ax.scatter(f[:, 0], f[:, 1], alpha=0.5)
-# plt.show()
+
 
